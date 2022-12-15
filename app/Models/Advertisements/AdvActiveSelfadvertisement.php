bd74fc6db4e4686b4bd6c6870944a8a4031057ad
frameworkfuturistic <anshu8581@gmail.com>
2022-12-15 18:22:20 +0530

    Lumen to Laravel

---
+++ app/Models/Advertisements/AdvActiveSelfadvertisement.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models\Advertisements;
+
+use Illuminate\Database\Eloquent\Model;
+
+class AdvActiveSelfadvertisement extends Model
+{
+    //
+}
---
+++ app/Repositories/SelfAdvets/iSelfAdvetRepo.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Repositories\SelfAdvets;
+
+/**
+ * | Created On-15-12-2022 
+ * | Created By-Anshu Kumar
+ * | Interface for Self Advertisement Repository
+ */
+interface iSelfAdvetRepo
+{
+    public function store($req);            // Store Data In Self Advertisements
+}
