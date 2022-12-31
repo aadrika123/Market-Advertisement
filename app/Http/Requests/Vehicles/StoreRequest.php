c15e4c8d51d774033bc15cb04bd1c25f36f7226d
frameworkfuturistic <anshu8581@gmail.com>
2022-12-31 16:35:42 +0530

    Validation

--- app/Http/Requests/Vehicles/StoreRequest.php
+++ app/Http/Requests/Vehicles/StoreRequest.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Requests\Vehicles;
 
+use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
 
 class StoreRequest extends FormRequest
 {
@@ -24,7 +26,44 @@ class StoreRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'applicant' => 'required',
+            'father' => 'required',
+            'email' => 'required|email',
+            'residenceAddress' => 'required',
+            'wardId' => 'required|integer',
+            'permanentAddress' => 'required',
+            'permanentWardId' => 'required|integer',
+            'mobile' => 'required|numeric|digits:10',
+            'aadharNo' => 'required|numeric|digits:12',
+            'licenseFrom' => 'required|date_format:Y-m-d',
+            'licenseTo' => 'required|date_format:Y-m-d',
+            'entityName' => 'required',
+            'tradeLicenseNo' => 'nullable|string',
+            'gstNo' => 'required|string',
+            'vehicleName' => 'required|string',
+            'vehicleNo' => 'required|string',
+            'vehicleType' => 'required|integer',
+            'ulbId' => 'required|integer',
+            'brandDisplayed' => 'required|string',
+            'frontArea' => 'required|numeric',
+            'rearArea' => 'required|numeric',
+            'topArea' => 'required|numeric',
+            'displayType' => 'required|integer',
+            'aadharDoc' => 'required|mimes:png,jpeg,pdf,jpg',
+            'tradeDoc' => 'nullable|mimes:png,jpeg,pdf,jpg',
+            'vehiclePhotoDoc' => 'nullable|mimes:png,jpeg,pdf,jpg',
+            'ownerBookDoc' => 'nullable|mimes:png,jpeg,pdf,jpg',
+            'insuranceDoc' => 'nullable|mimes:png,jpeg,pdf,jpg',
+            'gstDoc' => 'nullable|mimes:png,jpeg,pdf,jpg'
         ];
+    }
+
+    public function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'success'   => false,
+            'message'   => 'Validation errors',
+            'data'      => $validator->errors()
+        ], 422),);
     }
 }
