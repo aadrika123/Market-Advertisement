d83aea4a324d8ffbb040ea1964c8121ff4487386
mrinal9911 <mrinalk123456@gmail.com>
2023-07-01 15:55:23 +0530

    marriage apply

--- app/Http/Controllers/Marriage/MarriageRegistrationController.php
+++ app/Http/Controllers/Marriage/MarriageRegistrationController.php
@@ -4,12 +4,15 @@ namespace App\Http\Controllers\Marriage;
 
 use App\Http\Controllers\Controller;
 use App\MicroServices\IdGenerator\PrefixIdGenerator;
+use App\Models\Marriage\MarriageActiveRegistration;
+use App\Models\Workflows\WfRoleusermap;
 use App\Models\Workflows\WfWorkflow;
+use App\Models\Workflows\WfWorkflowrolemap;
 use App\Traits\Workflow\Workflow;
-use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\DB;
+use Exception;
 
 class MarriageRegistrationController extends Controller
 {
@@ -27,6 +30,7 @@ class MarriageRegistrationController extends Controller
     private $_applicationType;
     private $_applyMode;
     private $_tranType;
+    private $_registrarRoleId;
     # Class constructer 
     public function __construct()
     {
@@ -34,7 +38,7 @@ class MarriageRegistrationController extends Controller
         $this->_marriageParamId     = Config::get("marriage.PARAM_ID");
         $this->_marriageModuleId    = Config::get('marriage.MODULE_ID');
         $this->_userType            = Config::get("marriage.REF_USER_TYPE");
-        $this->_marriageWfRoles     = Config::get("marriage.ROLE_LABEL");
+        $this->_registrarRoleId     = Config::get("marriage.REGISTRAR_ROLE_ID");
         $this->_docReqCatagory      = Config::get("marriage.DOC_REQ_CATAGORY");
         $this->_dbKey               = Config::get("marriage.DB_KEYS");
         $this->_fee                 = Config::get("marriage.FEE_CHARGES");
@@ -49,80 +53,86 @@ class MarriageRegistrationController extends Controller
     {
         try {
             $mWfWorkflow = new WfWorkflow();
+            $mMarriageActiveRegistration = new MarriageActiveRegistration();
+            $mWfRoleusermaps = new WfRoleusermap();
             $user                       = authUser();
             $ulbId                      = $user->ulb_id ?? $req->ulbId;
+            $userType                   = $user->user_type;
             $workflowMasterId           = $this->_workflowMasterId;
             $marriageParamId            = $this->_marriageParamId;
             $feeId                      = $this->_fee;
-            $confApplicationType        = $this->_applicationType;
+            $registrarRoleId            = $this->_registrarRoleId;
 
-            # Get iniciater and finisher for the workflow 
+            # Get initiator and finisher for the workflow 
             $ulbWorkflowId = $mWfWorkflow->getulbWorkflowId($workflowMasterId, $ulbId);
             if (!$ulbWorkflowId) {
                 throw new Exception("Respective Ulb is not maped to 'marriage Registration' Workflow!");
             }
             $registrationCharges = 100;
-            $refInitiatorRoleId = $this->getInitiatorId($ulbWorkflowId->id);
-            $refFinisherRoleId  = $this->getFinisherId($ulbWorkflowId->id);
-            $finisherRoleId     = collect(DB::select($refFinisherRoleId))->first()->role_id;
-            $initiatorRoleId    = collect(DB::select($refInitiatorRoleId))->first()->role_id;
+            $refInitiatorRoleId  = $this->getInitiatorId($ulbWorkflowId->id);
+            $refFinisherRoleId   = $this->getFinisherId($ulbWorkflowId->id);
+            $mreqs = [
+                "roleId" => $registrarRoleId,
+                "ulbId"  => $ulbId
+            ];
+            $registrarId         = $mWfRoleusermaps->getUserId($mreqs);
+            $finisherRoleId      = collect(DB::select($refFinisherRoleId))->first();
+            $initiatorRoleId     = collect(DB::select($refInitiatorRoleId))->first();
+            if ($userType == 'Citizen') {
+                $initiatorRoleId = collect($initiatorRoleId)['forward_role_id'];         // Send to DA in Case of Citizen
+                $userId = null;
+                $citizenId = $user->id;
+            }
 
-            # Data Base interaction 
-            DB::beginTransaction();
             $idGeneration = new PrefixIdGenerator($marriageParamId, $ulbId);
             $marriageApplicationNo = $idGeneration->generate();
             $refData = [
-                "finisherRoleId"    => $finisherRoleId,
-                "initiatorRoleId"   => $initiatorRoleId,
+                "finisherRoleId"    => collect($finisherRoleId)['role_id'],
+                // "initiatorRoleId"   => collect($initiatorRoleId)['role_id'],
                 "workflowId"        => $ulbWorkflowId->id,
                 "applicationNo"     => $marriageApplicationNo,
+                "userId"            => $userId,
+                "citizenId"         => $citizenId,
+                "registrarId"       => $registrarId->user_id,
             ];
-            return $req->merge($refData);
+            $req->merge($refData);
 
-            # Renewal and the New Registration
-            if ($req->isRenewal == 0 || !isset($req->isRenewal)) {
-                if (isset($req->registrationId)) {
-                    throw new Exception("Registration No is Not Req for new marriage Registraton!");
-                }
-                $refData = [
-                    "applicationType" => "New_Apply",
-                    "applicationTypeId" => $confApplicationType['NEW_APPLY']
-                ];
-                $req->merge($refData);
-            }
-            if ($req->isRenewal == 1) {
-                $refData = [
-                    "applicationType" => "Renewal",
-                    "registrationId"  => $req->registrationId,
-                    "applicationTypeId" => $confApplicationType['RENEWAL']
-                ];
-                $req->merge($refData);
-                $mmarriageApprovedRegistration->deactivateOldRegistration($req->registrationId);
-            }
             # Save active details 
-            $applicationDetails = $mmarriageActiveRegistration->saveRegistration($req, $user);
-            $mmarriageActiveApplicant->saveApplicants($req, $applicationDetails['id']);
-            $mmarriageActiveDetail->savemarriageDetails($req, $applicationDetails['id']);
-
-            # Save registration charges
-            $metaRequest = new Request([
-                "applicationId"     => $applicationDetails['id'],
-                "applicationType"   => $req->applicationType,
-                "amount"            => $registrationCharges->amount,
-                "registrationFee"   => $registrationCharges->amount,
-                "applicationTypeId" => $req->applicationTypeId
-            ]);
-            $mmarriageRegistrationCharge->saveRegisterCharges($metaRequest);
-            DB::commit();
+            $applicationDetails = $mMarriageActiveRegistration->saveRegistration($req, $user);
 
             $returnData = [
                 "id" => $applicationDetails['id'],
                 "applicationNo" => $applicationDetails['applicationNo'],
             ];
-            return responseMsgs(true, "marriage Registration application submitted!", $returnData, "", "01", ".ms", "POST", $req->deviceId);
+            return responseMsgs(true, "Marriage Registration Application Submitted!", $returnData, "", "01", ".ms", "POST", $req->deviceId);
         } catch (Exception $e) {
-            DB::rollBack();
             return responseMsgs(false, $e->getMessage(), [], "", "01", ".ms", "POST", $req->deviceId);
+        }
+    }
+
+    /**
+     * | Registrar Inbox
+     */
+    public function inbox(Request $req)
+    {
+        try {
+            $userId = authUser()->id;
+            $ulbId = authUser()->ulb_id;
+            $mWfWorkflowRoleMaps = new WfWorkflowrolemap();
+            $perPage = $req->perPage ?? 10;
+
+            $roleId = $this->getRoleIdByUserId($userId)->pluck('wf_role_id');
+            $workflowIds = $mWfWorkflowRoleMaps->getWfByRoleId($roleId)->pluck('workflow_id');
+
+            $objection = $this->inboxList($workflowIds)
+                ->where('marriage_active_registrations.ulb_id', $ulbId)
+                ->whereIn('marriage_active_registrations.current_role', $roleId)
+                ->orderByDesc('marriage_active_registrations.id')
+                ->paginate($perPage);
+
+            return responseMsgs(true, "", remove_null($objection), '010805', '01', responseTime(), 'Post', '');
+        } catch (Exception $e) {
+            return responseMsgs(false, $e->getMessage(), "");
         }
     }
 }
--- config/marriage.php
+++ config/marriage.php
@@ -3,5 +3,6 @@
 return [
     "MODULE_ID"          => 10,
     "WORKFLOW_MASTER_ID" => 32,
-    "PARAM_ID"           => 38
+    "PARAM_ID"           => 38,
+    "REGISTRAR_ROLE_ID"  => 36
 ];
