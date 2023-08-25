a04e830232a7ce2f2014417fcd2303ef650f638d
bikash05 <68511641+bikash05@users.noreply.github.com>
2023-08-25 18:54:46 +0530

    Update constants.php

--- config/constants.php
+++ config/constants.php
@@ -14,7 +14,8 @@ return [
     // "BASE_URL" => 'http://192.168.0.21:8005',         
     // "PAYMENT_URL" => 'http://192.168.0.202:8006/',  
 
-    "ULB_LOGO_URL" => 'http://192.168.0.202:8000/',
+    "ULB_LOGO_URL" => 'http://192.168.0.202:8001/',
+
     "AADHAR_RELATIVE_NAME" => "AADHAR",
     "TRADE_RELATIVE_NAME" => "TRADE",
     "HOLDING_RELATIVE_NAME" => "HOLDING",
