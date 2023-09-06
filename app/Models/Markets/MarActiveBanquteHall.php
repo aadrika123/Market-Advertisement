1c017ccd8209c6c789ed78c2dc8fc93ca8f24cc3
bikash05 <68511641+bikash05@users.noreply.github.com>
2023-09-06 16:37:55 +0530

    update doc upload api in market section

--- app/Models/Markets/MarActiveBanquteHall.php
+++ app/Models/Markets/MarActiveBanquteHall.php
@@ -208,12 +208,12 @@ class MarActiveBanquteHall extends Model
             $a = new Request($metaReqs);
             // $mWfActiveDocument->postDocuments($a,$auth);
             $metaReqs =  $mWfActiveDocument->metaReqs($metaReqs);
-            // $mWfActiveDocument->create($metaReqs);
-            foreach($metaReqs as $key=>$val)
-            {
-                $mWfActiveDocument->$key = $val;
-            }
-            $mWfActiveDocument->save();
+            $mWfActiveDocument->create($metaReqs);
+            // foreach($metaReqs as $key=>$val)
+            // {
+            //     $mWfActiveDocument->$key = $val;
+            // }
+            // $mWfActiveDocument->save();
         });
     }
 
