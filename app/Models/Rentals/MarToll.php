4b7fb9b0317c2bc875d75320820193cc9a3b5385
bikash05 <68511641+bikash05@users.noreply.github.com>
2023-11-17 14:12:10 +0530

    Update MarToll.php

--- app/Models/Rentals/MarToll.php
+++ app/Models/Rentals/MarToll.php
@@ -94,12 +94,12 @@ class MarToll extends Model
             'mar_tolls.*',
             'mc.circle_name',
             'mm.market_name',
-            DB::raw("TO_CHAR(msp.payment_date, 'DD-MM-YYYY') as last_payment_date"),
-            'msp.amount as last_payment_amount'
+            DB::raw("TO_CHAR(mtp.payment_date, 'DD-MM-YYYY') as last_payment_date"),
+            'mtp.amount as last_payment_amount'
         )
             ->join('m_circle as mc', 'mar_tolls.circle_id', '=', 'mc.id')
             ->join('m_market as mm', 'mar_tolls.market_id', '=', 'mm.id')
-            ->leftjoin('mar_shop_payments as msp', 'mar_tolls.last_tran_id', '=', 'msp.id')
+            ->leftjoin('mar_toll_payments as mtp', 'mar_tolls.last_tran_id', '=', 'mtp.id')
             ->where('mar_tolls.market_id', $marketid)
             ->where('mar_tolls.status', '1')
             ->orderByDesc('mar_tolls.id');
