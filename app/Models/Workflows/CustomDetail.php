ea5b06379069da80068db97a79ef558bb49c5020
mrinal9911 <mrinalk123456@gmail.com>
2024-05-09 10:28:19 +0530

    commt

--- app/Models/Workflows/CustomDetail.php
+++ app/Models/Workflows/CustomDetail.php
@@ -19,8 +19,8 @@ class CustomDetail extends Model
             'id',
             'ref_id',
             'ref_type',
-            'relative_path',
-            'doc_name as docUrl',
+            // 'relative_path',
+            // 'doc_name as docUrl',
             'remarks',
             'type',
             'created_at as date',
@@ -30,10 +30,34 @@ class CustomDetail extends Model
             ->where('ref_id', $request->applicationId)
             ->where('ref_type', trim(strtoupper($request->customFor)))
             ->get();
-        $customDetails = $customDetails->map(function ($val) {
-            $path = config('app.url') . '/' . $val->relative_path . '/' . $val->docUrl;
-            $val->docUrl = $path;
-            return $val;
-        });
+        // $customDetails = $customDetails->map(function ($val) {
+        //     $path = config('app.url') . '/' . $val->relative_path . '/' . $val->docUrl;
+        //     $val->docUrl = $path;
+        //     return $val;
+        // });
+
+        // $docUpload = new DocUpload;
+        // $customDetails = CustomDetail::select(
+        //     'id',
+        //     'ref_id',
+        //     'ref_type',
+        //     'doc_ref_no as reference_no',
+        //     'doc_unique_id',
+        //     'remarks',
+        //     'type',
+        //     'created_at as date',
+        //     'ref_type as customFor'
+        // )
+        //     ->where('ref_id', $request->applicationId)
+        //     ->where('ref_type', trim(strtoupper($request->customFor)))
+        //     ->orderByDesc('id')
+        //     ->get();
+
+        // $customDetails = $customDetails->map(function ($val) use ($docUpload) {
+        //     $docResponse =  $docUpload->getSingleDocUrl($val);
+        //     $val->docUrl =  $docResponse['doc_path'] ?? null;
+        //     return $val;
+        // });
+        // return responseMsg(true, "Data Retrieved", $customDetails);
     }
 }
