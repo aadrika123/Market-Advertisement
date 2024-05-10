c563d130d3ab60c9c57eae939ca40bb04b94f507
mrinal9911 <mrinalk123456@gmail.com>
2024-05-10 13:47:39 +0530

    ""

--- app/Http/Controllers/Pet/PetWorkflowController.php
+++ app/Http/Controllers/Pet/PetWorkflowController.php
@@ -316,7 +316,7 @@ class PetWorkflowController extends Controller
             //     'forward_time' => $current->format('H:i:s')
             // ]);
             $this->commit();
-            return responseMsgs(true, $msg, null, "", "", '01', responseTime(), 'Post', '');
+            return responseMsgs(true, $msg, "", "", "", '01', responseTime(), 'Post', '');
         } catch (Exception $e) {
             $this->rollback();
             return responseMsgs(false, $e->getMessage(), [], "", "01", responseTime(), "POST", $req->deviceId);
