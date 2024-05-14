3b97f68ca3ea766800b536de9758eea5e608c1f6
pandeyprity <pandeyprity33@gmail.com>
2024-05-14 18:12:59 +0530

    user nme in SI level

--- app/Models/Workflows/WorkflowTrack.php
+++ app/Models/Workflows/WorkflowTrack.php
@@ -64,7 +64,7 @@ class WorkflowTrack extends Model
      */
     public function getTracksByRefId($mRefTable, $tableId)
     {
-      return WorkflowTrack::select(
+        return WorkflowTrack::select(
             'workflow_tracks.ref_table_dot_id AS referenceTable',
             'workflow_tracks.ref_table_id_value AS applicationId',
             'workflow_tracks.message',
@@ -72,12 +72,15 @@ class WorkflowTrack extends Model
             'workflow_tracks.forward_date',
             'workflow_tracks.forward_time',
             'w.role_name as commentedBy',
-            'wr.role_name as forwarded_to'
+            'wr.role_name as forwarded_to',
+            'users.name',
+            'users.user_code',
         )
             ->where('ref_table_dot_id', $mRefTable)
             ->where('ref_table_id_value', $tableId)
             ->join('wf_roles as w', 'w.id', '=', 'workflow_tracks.sender_role_id')
             ->leftJoin('wf_roles as wr', 'wr.id', '=', 'workflow_tracks.receiver_role_id')
+            ->join('users', 'users.id', '=', 'workflow_tracks.user_id')
             ->where('citizen_id', null)
             ->orderByDesc('workflow_tracks.id')
             ->get();
