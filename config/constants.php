e5f253188387a37dba7339cb0003992a0a3e35da
pandeyprity <pandeyprity33@gmail.com>
2024-05-28 17:35:19 +0530

    receipt changes

--- config/constants.php
+++ config/constants.php
@@ -14,8 +14,9 @@ return [
     "PAYMENT_URL" => env('PAYMENT_URL','http://localhost:8000/'),
     "BASE_URL" => env('BASE_URL','http://localhost/'),
     "95_PAYMENT_URL" => env('95_PAYMENT_URL','http://localhost:8000/'),    
-    "ULB_LOGO_URL" => env('ULB_LOGO_URL','http://localhost/'),                       // Property URL
-    "DOC_URL" => env('DOC_URL','http://localhost/'),                       // Property URL
+    //"ULB_LOGO_URL" => env('ULB_LOGO_URL','http://localhost/'),                       // Property URL
+    "DOC_URL" => env('DOC_URL','http://localhost/'),   
+    "ULB_LOGO_URL"    => env('ULB_LOGO_URL'),                    // Property URL
 
     // Local URL
     // "AUTH_URL" => 'http://192.168.0.21:8005/',    
