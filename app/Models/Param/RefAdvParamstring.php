50e105bb0e590b6109533bd89ad58cbf97b29fdc
pandeyprity <pandeyprity33@gmail.com>
2024-06-22 13:56:29 +0530

    string

--- app/Models/Param/RefAdvParamstring.php
+++ app/Models/Param/RefAdvParamstring.php
@@ -12,19 +12,34 @@ class RefAdvParamstring extends Model
     protected $guarded = [];
 
     // Get all Masters 
+    // public function masters()
+    // {
+    //     return DB::table('ref_adv_paramstrings')
+    //         ->select(
+    //             "ref_adv_paramstrings.id",
+    //             DB::raw('CONCAT(UPPER(SUBSTRING(ref_adv_paramstrings.string_parameter,1,1)),LOWER(SUBSTRING(ref_adv_paramstrings.string_parameter,2))) as string_parameter'),
+    //             // "ref_adv_paramstrings.string_parameter",
+    //             "c.param_category",
+    //             "ref_adv_paramstrings.param_category_id"
+    //         )
+    //         ->leftJoin('ref_adv_paramcategories as c', 'c.id', '=', 'ref_adv_paramstrings.param_category_id')
+    //         ->where('ref_adv_paramstrings.status','1')
+    //         ->orderBy('ref_adv_paramstrings.id','Asc')
+    //         ->get();
+    // }
+
     public function masters()
     {
         return DB::table('ref_adv_paramstrings')
             ->select(
                 "ref_adv_paramstrings.id",
-                DB::raw('CONCAT(UPPER(SUBSTRING(ref_adv_paramstrings.string_parameter,1,1)),LOWER(SUBSTRING(ref_adv_paramstrings.string_parameter,2))) as string_parameter'),
-                // "ref_adv_paramstrings.string_parameter",
+                DB::raw('(ref_adv_paramstrings.string_parameter) as string_parameter'),
                 "c.param_category",
                 "ref_adv_paramstrings.param_category_id"
             )
             ->leftJoin('ref_adv_paramcategories as c', 'c.id', '=', 'ref_adv_paramstrings.param_category_id')
-            ->where('ref_adv_paramstrings.status','1')
-            ->orderBy('ref_adv_paramstrings.id','Asc')
+            ->where('ref_adv_paramstrings.status', '1')
+            ->orderBy('ref_adv_paramstrings.id', 'Asc')
             ->get();
     }
 }
