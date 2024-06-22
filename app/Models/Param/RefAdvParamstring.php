6b1e4f5d8b19e9d60a4ab85cee69d188a4c133ea
pandeyprity <pandeyprity33@gmail.com>
2024-06-22 14:02:32 +0530

    revert changes

--- app/Models/Param/RefAdvParamstring.php
+++ app/Models/Param/RefAdvParamstring.php
@@ -11,35 +11,22 @@ class RefAdvParamstring extends Model
     use HasFactory;
     protected $guarded = [];
 
-    // Get all Masters 
-    // public function masters()
-    // {
-    //     return DB::table('ref_adv_paramstrings')
-    //         ->select(
-    //             "ref_adv_paramstrings.id",
-    //             DB::raw('CONCAT(UPPER(SUBSTRING(ref_adv_paramstrings.string_parameter,1,1)),LOWER(SUBSTRING(ref_adv_paramstrings.string_parameter,2))) as string_parameter'),
-    //             // "ref_adv_paramstrings.string_parameter",
-    //             "c.param_category",
-    //             "ref_adv_paramstrings.param_category_id"
-    //         )
-    //         ->leftJoin('ref_adv_paramcategories as c', 'c.id', '=', 'ref_adv_paramstrings.param_category_id')
-    //         ->where('ref_adv_paramstrings.status','1')
-    //         ->orderBy('ref_adv_paramstrings.id','Asc')
-    //         ->get();
-    // }
-
+    //Get all Masters 
     public function masters()
     {
         return DB::table('ref_adv_paramstrings')
             ->select(
                 "ref_adv_paramstrings.id",
-                DB::raw('(ref_adv_paramstrings.string_parameter) as string_parameter'),
+                DB::raw('CONCAT(UPPER(SUBSTRING(ref_adv_paramstrings.string_parameter,1,1)),LOWER(SUBSTRING(ref_adv_paramstrings.string_parameter,2))) as string_parameter'),
+                // "ref_adv_paramstrings.string_parameter",
                 "c.param_category",
                 "ref_adv_paramstrings.param_category_id"
             )
             ->leftJoin('ref_adv_paramcategories as c', 'c.id', '=', 'ref_adv_paramstrings.param_category_id')
-            ->where('ref_adv_paramstrings.status', '1')
-            ->orderBy('ref_adv_paramstrings.id', 'Asc')
+            ->where('ref_adv_paramstrings.status','1')
+            ->orderBy('ref_adv_paramstrings.id','Asc')
             ->get();
     }
+
+    
 }
