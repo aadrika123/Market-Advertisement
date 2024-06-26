b7af278eedac795b9a7dc227c6bd00876e1c424b
pandeyprity <pandeyprity33@gmail.com>
2024-06-26 17:39:38 +0530

    image

--- app/Models/Advertisements/WfActiveDocument.php
+++ app/Models/Advertisements/WfActiveDocument.php
@@ -128,7 +128,7 @@ class WfActiveDocument extends Model
     {
         $data = WfActiveDocument::select('*', DB::raw("replace(doc_code,'_',' ') as doc_val"), DB::raw("CONCAT(wf_active_documents.relative_path,'/',wf_active_documents.document) as doc_path"))
             ->where(['active_id' => $appId, 'workflow_id' => $workflowId])
-            ->where('current_status', [0,1])
+            ->where('current_status', '1')
             ->get();
         return $data;
     }
