099d39a7823a06410821e2521245de719aac50d7
pandeyprity <pandeyprity33@gmail.com>
2024-06-27 12:47:07 +0530

    corrected

--- routes/api.php
+++ routes/api.php
@@ -432,10 +432,10 @@ Route::group(['middleware' => ['checkToken']], function () {
         Route::post('market/bm-hall/list-btc-inbox-jsk', 'listBtcInboxJsk');
         Route::post('market/bm-hall/reupload-document', 'reuploadDocument');
 
-        Route::post('market/lodge/btc/get-details-by-id', 'getRejectedDetailsById');
-        Route::post('market/lodge/list-btc-inbox-jsk', 'listBtcInboxJsk');
-        Route::post('market/lodge/view-btc-document', 'getUploadDocumentsBtc');
-        Route::post('market/lodge/reupload-document', 'reuploadDocument');
+        // Route::post('market/lodge/btc/get-details-by-id', 'getRejectedDetailsById');
+        // Route::post('market/lodge/list-btc-inbox-jsk', 'listBtcInboxJsk');
+        // Route::post('market/lodge/view-btc-document', 'getUploadDocumentsBtc');
+        // Route::post('market/lodge/reupload-document', 'reuploadDocument');
     });
 
     /**
