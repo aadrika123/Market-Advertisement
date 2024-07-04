3cf0ab541086a1e24cc87523f8d5e0f7e53e6188
mrinal9911 <mrinalk123456@gmail.com>
2024-07-04 16:00:50 +0530

    ok

--- config/constants.php
+++ config/constants.php
@@ -18,6 +18,10 @@ return [
     "DOC_URL" => env('DOC_URL','http://localhost/'),   
     "ULB_LOGO_URL"    => env('ULB_LOGO_URL'),                    // Property URL
 
+    "WHATSAPP_TOKEN"        => env("WHATSAPP_TOKEN", "xxx"),
+    "WHATSAPP_NUMBER_ID"    => env("WHATSAPP_NUMBER_ID", "xxx"),
+    "WHATSAPP_URL"          => env("WHATSAPP_URL", "xxx"),
+
     // Local URL
     // "AUTH_URL" => 'http://192.168.0.21:8005/',    
     // "BASE_URL" => 'http://192.168.0.21:8005',         
