4c8234d262a2add6b1c5709121b000ba9895a8c3
mrinal9911 <mrinalk123456@gmail.com>
2024-09-11 16:15:12 +0530

    changes

--- app/Http/Controllers/Advertisements/AgencyController.php
+++ app/Http/Controllers/Advertisements/AgencyController.php
@@ -584,7 +584,7 @@ class AgencyController extends Controller
         //     return $value;
         // });
         $data = (new DocumentUpload())->getDocUrl($data);
-        return $data;
+        return responseMsgs(true, "Uploaded Documents", $data, "010102", "1.0", "", "POST", $req->deviceId ?? "");
     }
 
 
