188d1d09f887fb9b36f653f55bacc1a1569908ac
mrinal9911 <mrinalk123456@gmail.com>
2024-09-19 17:19:44 +0530

    changes

--- app/Models/Advertisements/AdvActiveHoarding.php
+++ app/Models/Advertisements/AdvActiveHoarding.php
@@ -268,9 +268,9 @@ class AdvActiveHoarding extends Model
                     'tm.type_inner as hoardingCategory',
                     'p.string_parameter as licenseYear',
                     DB::raw("CASE 
-                    WHEN adv_rejected_hoardings.current_role_id = 6 THEN 'AT LIPIK'
+                    WHEN adv_rejected_hoardings.current_role_id = 6 THEN 'AT DEALING ASSISTANT'
                     WHEN adv_rejected_hoardings.current_role_id = 9 THEN 'SECTION INCHARGE'
-                    WHEN adv_rejected_hoardings.current_role_id = 10 THEN 'AT TAX SUPRERINTENDENT'
+                    WHEN adv_rejected_hoardings.current_role_id = 10 THEN 'AT EXECUTIVE OFFICER'
                     ELSE 'Unknown Role'
                     END AS application_at")
                 )
@@ -306,9 +306,9 @@ class AdvActiveHoarding extends Model
                     'tm.type_inner as hoardingCategory',
                     'p.string_parameter as licenseYear',
                     DB::raw("CASE 
-                    WHEN adv_hoardings.current_role_id = 6 THEN 'AT LIPIK'
+                    WHEN adv_hoardings.current_role_id = 6 THEN 'AT DEALING ASSISTANT'
                     WHEN adv_hoardings.current_role_id = 9 THEN 'SECTION INCHARGE'
-                    WHEN adv_hoardings.current_role_id = 10 THEN 'AT TAX SUPRERINTENDENT'
+                    WHEN adv_hoardings.current_role_id = 10 THEN 'AT EXECUTIVE OFFICER'
                     ELSE 'Unknown Role'
                     END AS application_at")
                 )
